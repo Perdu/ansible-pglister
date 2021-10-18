6e19b269617bea34e6a0c377407b02c6e1724c1c
Célestin Matte <gitlab@cmatte.me>
2021-10-18 18:21:44 +0200

    pgarchives: simplify modification of schema.sql

--- roles/pgarchives/files/schema.sql
+++ roles/pgarchives/files/schema.sql
@@ -6,86 +6,58 @@ ALTER TABLE messages
    ADD COLUMN rawtxt bytea NOT NULL,
    ADD COLUMN fti tsvector NOT NULL;
 
-CREATE INDEX IF NOT EXISTS idx_messages_threadid ON messages(threadid);
-CREATE UNIQUE INDEX IF NOT EXISTS idx_messages_msgid ON messages(messageid);
-CREATE INDEX IF NOT EXISTS idx_messages_date ON messages(date);
-CREATE INDEX IF NOT EXISTS idx_messages_parentid ON messages(parentid);
+CREATE INDEX idx_messages_threadid ON messages(threadid);
+CREATE UNIQUE INDEX idx_messages_msgid ON messages(messageid);
+CREATE INDEX idx_messages_date ON messages(date);
+CREATE INDEX idx_messages_parentid ON messages(parentid);
 
-CREATE TABLE IF NOT EXISTS message_hide_reasons (
+CREATE TABLE message_hide_reasons (
    message int NOT NULL PRIMARY KEY REFERENCES messages,
    dt timestamptz,
    reason text,
    by text
 );
 
-CREATE SEQUENCE IF NOT EXISTS threadid_seq;
+CREATE SEQUENCE threadid_seq;
 
-CREATE TABLE IF NOT EXISTS unresolved_messages(
+CREATE TABLE unresolved_messages(
    message int NOT NULL REFERENCES messages,
    priority int NOT NULL,
    msgid text NOT NULL,
    CONSTRAINT unresolved_messages_pkey PRIMARY KEY (message, priority)
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS idx_unresolved_msgid_message ON unresolved_messages(msgid, message);
+CREATE UNIQUE INDEX idx_unresolved_msgid_message ON unresolved_messages(msgid, message);
 
-CREATE TABLE IF NOT EXISTS listgroups(
-   groupid int NOT NULL PRIMARY KEY,
-   groupname text NOT NULL UNIQUE,
-   sortkey int NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS lists(
-   listid int NOT NULL PRIMARY KEY,
-   listname text NOT NULL UNIQUE,
-   shortdesc text NOT NULL,
-   description text NOT NULL,
-   active boolean NOT NULL,
-   groupid int NOT NULL REFERENCES listgroups(groupid)
-);
-
-CREATE TABLE IF NOT EXISTS list_months(
+CREATE TABLE list_months(
    listid int NOT NULL REFERENCES lists(listid),
    year int NOT NULL,
    month int NOT NULL,
    CONSTRAINT list_months_pk PRIMARY KEY (listid, year, month)
 );
 
-CREATE TABLE IF NOT EXISTS list_threads(
+CREATE TABLE list_threads(
    threadid int NOT NULL, /* comes from threadid_seq */
    listid int NOT NULL REFERENCES lists(listid),
    CONSTRAINT pg_list_threads PRIMARY KEY (threadid, listid)
 );
-CREATE INDEX IF NOT EXISTS list_threads_listid_idx ON list_threads(listid);
+CREATE INDEX list_threads_listid_idx ON list_threads(listid);
 
-CREATE TABLE IF NOT EXISTS attachments(
-   id serial not null primary key,
-   message int not null references messages(id),
-   filename text not null,
-   contenttype text not null,
-   attachment bytea not null
-);
-CREATE INDEX IF NOT EXISTS idx_attachments_msg ON attachments(message);
+CREATE INDEX idx_attachments_msg ON attachments(message);
 
-CREATE TABLE IF NOT EXISTS apiclients(
+CREATE TABLE apiclients(
    id SERIAL NOT NULL PRIMARY KEY,
    apikey varchar(100) NOT NULL,
    postback varchar(500) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS threadsubscriptions(
-   id SERIAL NOT NULL PRIMARY KEY,
-   apiclient_id integer NOT NULL REFERENCES apiclients(id),
-   threadid integer NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS threadnotifications(
+CREATE TABLE threadnotifications(
    apiclient_id integer NOT NULL REFERENCES apiclients(id),
    threadid integer NOT NULL,
    CONSTRAINT threadnotifications_pkey PRIMARY KEY (apiclient_id, threadid)
 );
 
-CREATE TABLE IF NOT EXISTS loaderrors(
+CREATE TABLE loaderrors(
    id SERIAL NOT NULL PRIMARY KEY,
    listid int NOT NULL,
    dat timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -130,7 +102,7 @@ $$ LANGUAGE 'plpgsql';
 CREATE TRIGGER messages_fti_trigger
  BEFORE INSERT OR UPDATE OF subject, bodytxt ON  messages
  FOR EACH ROW EXECUTE PROCEDURE messages_fti_trigger_func();
-CREATE INDEX IF NOT EXISTS messages_fti_idx ON messages USING gin(fti);
+CREATE INDEX messages_fti_idx ON messages USING gin(fti);
 
 CREATE OR REPLACE FUNCTION messages_notify_threads_trg_func() RETURNS trigger AS $$
 BEGIN
@@ -149,7 +121,7 @@ CREATE TRIGGER messages_notify_trigger
  AFTER INSERT ON messages
  FOR EACH ROW EXECUTE PROCEDURE messages_notify_threads_trg_func();
 
-CREATE TABLE IF NOT EXISTS legacymap(
+CREATE TABLE legacymap(
        listid int not null,
        year int not null,
        month int not null,
